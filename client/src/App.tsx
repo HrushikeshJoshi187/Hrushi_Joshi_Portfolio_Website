803535a5b09467f7805b21aa914a83c448b39212
Hrushikesh Joshi <Hrushikesh.joshi.187@gmail.com>
2025-02-03 20:11:23 -0600

    Changes 2025-02-03
    removed errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ReactLenis } from "lenis/react";
 import { gsap } from "gsap";
